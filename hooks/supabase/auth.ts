830acaa20567b62141fe24381e1ffc527ede97ac
Kokohachi <99192596+Kokohachi@users.noreply.github.com>
2023-09-15 23:14:39 +0900

    Add: Hooks

---
+++ hooks/supabase/auth.ts
@@ -0,0 +1,96 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true,
+      storage: localStorage? localStorage : undefined,
+    },
+  }
+);
+
+export function getSupabaseClient() {
+  return supabase;
+}
+
+export function getSupabaseAuth() {
+  return supabase.auth;
+}
+
+export function supabaseLogin() {
+  return supabase.auth.signInWithOAuth({
+    provider: "discord",
+    options: { scopes: "identify, email, guilds" },
+  });
+}
+
+export function supabaseLogout() {
+  return supabase.auth.signOut();
+}
+
+async function _supabaseGetUser() {
+  try {
+    const { data, error } = await supabase.auth.getSession();
+    if (data.session) {
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
+      if (userError) {
+        return null;
+      }
+      if (userData.user) {
+        return userData.user;
+      }
+    }
+    if (error) {
+      return null;
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+export function supabaseGetUser() {
+  return _supabaseGetUser().then((data) => {
+    return data;
+  })
+}
+
+async function _supabaseAddChart(chartID: string, title: string, artist: string, author: string, author_id: string, difficulty: number, description: string, event: string, tags: string, useHID: boolean, uid: string) {
+  const { data, error } = await supabase.from("charts").insert([
+    {
+      id: chartID,
+      title: title,
+      artist: artist,
+      author: author,
+      author_id: author_id,
+      difficulty: difficulty,
+      desc: description,
+      event: event,
+      tags: tags,
+      play_count: 0,
+      like_count: 0,
+      testing: true,
+      activated: true,
+      user: uid,
+      HID: useHID,
+      publish_at: null,
+    },
+  ]);
+  if (error) {
+    return null;
+  }
+  if (data) {
+    return data;
+  }
+}
+
+export function supabaseAddChart(chartID: string, title: string, artist: string, author: string, author_id: string, difficulty: number, description: string, event: string, tags: string, useHID: boolean, uid: string) {
+  _supabaseAddChart(chartID, title, artist, author, author_id, difficulty, description, event, tags, useHID, uid).then((data) => {
+    return data;
+  })
+}
